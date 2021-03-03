84a879569422d16fb08b8c848177b9810f264456
evochange <56960162+evochange@users.noreply.github.com>
2021-03-03 18:44:37 +0000

    Create angsd2sweed-input.R

---
+++ SweeD/angsd2sweed-input.R
@@ -0,0 +1,31 @@
+# PREPARE SWEED INPUT
+# FROM ANGSD MAFS FILE OUTPUT
+#
+# I. Miranda (03/2019)
+#
+# This script assumes that the ANGSD .mafs output was produced using the ancestral state as major allele (-doMaf 5).
+#
+
+#Import required library:
+library(tidyverse)
+
+#Set working directory:
+setwd ("/path/to/mafs-file-directory")
+
+#Read .mafs file:
+angsd_mafs <- read.table("angsd-output.mafs", header = T, row.names = NULL)
+
+#Calculate SweeD input columns:
+input_prep <- mutate(angsd_mafs, x = round(knownEM*nInd*2, digits = 0),   #x column = number of derived alleles sampled
+            n = nInd*2,  #n column = total number of alleles in the population
+            folded = 0)  #folded = 0; known ancestral state
+
+#Create dataframe with columns of interests:
+sweeD_input <- data.frame(input_prep$position, input_prep$x, input_prep$n, input_prep$folded)
+
+#Add column names to SweeD input:
+names(sweeD_input) <- c("position", "x", "n", "folded")
+
+#Output SweeD input file:
+write.table(sweeD_input, "SeeD_input_file.txt", col.names=T, row.names = F, quote = F, sep = "\t")
+
